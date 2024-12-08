74b92f4b1ccd0034890f4fd35ae9bc7c2cf94967
scottmk <michael.kelly.scott@gmail.com>
2024-12-07 19:49:36 -0800

    feat: finished Day 2 part 2
    I'm not crazy about brute-forcing it, but I think that there are too many edge cases for me to want to bother with finding a general solution, particularly when all the reports are very small

--- day_2/main.py
+++ day_2/main.py
@@ -1,6 +1,6 @@
 """https://adventofcode.com/2024/day/2"""
 
-from itertools import pairwise
+from itertools import groupby, pairwise
 from math import copysign
 from common_main_methods import read_text_file_lines
 
@@ -18,13 +18,27 @@ def is_report_safe(report):
     return is_safe
 
 
-def get_safe_report_count(input_filepath):
+def is_report_safe_with_dampener(report):
+    if is_report_safe(report):
+        return True
+    else:
+        for index_to_remove in range(1, len(report) + 1):
+            if is_report_safe(report[:index_to_remove-1] + report[index_to_remove:]):
+                return True
+    return False
+
+
+def get_safe_report_count(input_filepath, with_dampener=False):
     report_lines = read_text_file_lines(input_filepath)
     reports = [[int(level) for level in report.split()] for report in report_lines]
     
-    return sum([1 if is_report_safe(report) else 0 for report in reports])
+    if with_dampener:
+        return sum([1 if is_report_safe_with_dampener(report) else 0 for report in reports])
+    else:
+        return sum([1 if is_report_safe(report) else 0 for report in reports])
 
 
 if __name__ == '__main__':
     input_filepath = './day_2/input.txt'
     print(f"Number of Safe Reports: {get_safe_report_count(input_filepath)}")
+    print(f"Number of Safe Reports with Dampener: {get_safe_report_count(input_filepath, with_dampener=True)}")

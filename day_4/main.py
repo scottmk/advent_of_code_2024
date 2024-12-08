da937c0e6d54f916972ee8c8d9258e54bd0c49f0
scottmk <michael.kelly.scott@gmail.com>
2024-12-07 22:50:19 -0800

    feat: finished Day 4 part 2

--- day_4/main.py
+++ day_4/main.py
@@ -70,6 +70,39 @@ def count_xmas_occurrences(input_filepath):
     return possible_words['XMAS']
 
 
+def count_x_mas_occurrences(input_filepath):
+    grid = read_text_file_lines(input_filepath)
+    num_lines = len(grid)
+    valid_x_count = 0
+    for row_idx, line in enumerate(grid):
+        line = line.strip()
+        line_length = len(line)
+        for col_idx, letter in enumerate(line):
+            if letter != 'A':
+                continue
+            if not (0 < row_idx < num_lines - 1 and 0 < col_idx < line_length - 1):
+                continue
+            valid_x_count += _validate_x(grid, row_idx, col_idx, 'MMSS')  # M on top, S on bottom
+            valid_x_count += _validate_x(grid, row_idx, col_idx, 'SSMM')  # S on top, M on bottom
+            valid_x_count += _validate_x(grid, row_idx, col_idx, 'MSMS')  # M on left, S on right
+            valid_x_count += _validate_x(grid, row_idx, col_idx, 'SMSM')  # S on left, M on right
+    return valid_x_count
+
+
+def _validate_x(grid, row_idx, col_idx, x_type):
+    if (
+        grid[row_idx - 1][col_idx - 1] == x_type[0] and 
+        grid[row_idx - 1][col_idx + 1] == x_type[1] and
+        grid[row_idx + 1][col_idx - 1] == x_type[2] and
+        grid[row_idx + 1][col_idx + 1] == x_type[3]
+    ):
+        return 1
+    else: 
+        return 0
+
+
 if __name__ == '__main__':
     input_filepath = './day_4/input.txt'
     print(f"Number of XMAS in search: {count_xmas_occurrences(input_filepath)}")
+    print(f"Number of X-MAS in search: {count_x_mas_occurrences(input_filepath)}")
+    
